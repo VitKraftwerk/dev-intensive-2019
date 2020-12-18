d0aac8d0f014ec0a10bc94dd1c6fd88405e29c03
Vit <vit.kraftwerk@gmail.com>
2020-12-18 18:20:34 +0300

    Initials to drawable

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -7,8 +7,12 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import androidx.annotation.ColorRes
 import androidx.annotation.Dimension
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
+import ru.skillbranch.devintensive.App
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.repositories.PreferencesRepository
 import java.lang.Integer.parseInt
 
 
@@ -32,32 +36,33 @@ class CircleImageView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
     companion object {
-        private const val DEFAULT_DIMENSION = 1
+        private const val DEFAULT_DIMENSION = 2
         private const val DEFAULT_COLOR = Color.WHITE
     }
 
     private var borderWidth: Int = DEFAULT_DIMENSION
     private var color: Int = DEFAULT_COLOR
 
-    init{
-        if(attrs!= null){
+    init {
+        if (attrs != null) {
             val a = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView)
             borderWidth = a.getDimension(
                 R.styleable.CircleImageView_cv_borderWidth,
                 DEFAULT_DIMENSION.toFloat()
             ).toInt()
-            color= a.getInt(R.styleable.CircleImageView_cv_borderColor, DEFAULT_COLOR)
+            color = a.getInt(R.styleable.CircleImageView_cv_borderColor, DEFAULT_COLOR)
             a.recycle()
         }
     }
 
-    @Dimension fun getBorderWidth() : Int = borderWidth
+    @Dimension
+    fun getBorderWidth(): Int = borderWidth
 
     fun setBorderWidth(@Dimension dp: Int) {
         borderWidth = dp
     }
 
-    fun getBorderColor():Int = color
+    fun getBorderColor(): Int = color
 
     fun setBorderColor(hex: String) {
         color = parseInt(hex, 16)
@@ -73,14 +78,41 @@ class CircleImageView @JvmOverloads constructor(
         val w = width
         val h = height
 
-        var paint = Paint()
         val radius = w / 2f - borderWidth / 2f
 
-        paint.setStyle(Paint.Style.STROKE)
-        paint.setStrokeWidth(borderWidth.toFloat());
-        paint.setColor(color)
+        Paint().apply {
+            style = Paint.Style.STROKE
+            strokeWidth = borderWidth.toFloat()
+            color = this@CircleImageView.color
+            canvas!!.drawCircle(w / 2f, h / 2f, radius, this)
+        }
 
-        canvas!!.drawCircle( w / 2f, h / 2f, radius, paint)
+        val initials = PreferencesRepository.getProfile().firstName.trim().take(1)  +
+                PreferencesRepository.getProfile().lastName.trim().take(1)
+
+        if (!initials.isNullOrEmpty()) {
+
+            Paint().apply {
+                style = Paint.Style.FILL
+                color = if(PreferencesRepository.getAppTheme() == AppCompatDelegate.MODE_NIGHT_NO) {
+                    resources.getColor(R.color.color_accent, App.applicationContext().theme)
+                } else {
+                    resources.getColor(R.color.color_accent_night, App.applicationContext().theme)
+                }
+                canvas!!.drawCircle(w / 2f, h / 2f, radius, this)
+            }
+
+            val textPaint = Paint().apply {
+                textAlign = Paint.Align.CENTER
+                textSize = radius / 1.5f
+                color = DEFAULT_COLOR
+            }
+
+            val xPos = w / 2
+            //((textPaint.descent() + textPaint.ascent()) / 2) is the distance from the baseline to the center.
+            val yPos = (h / 2 - (textPaint.descent() + textPaint.ascent()) / 2).toInt()
+            canvas!!.drawText(initials.toUpperCase(), xPos.toFloat(), yPos.toFloat(), textPaint)
+        }
 
     }
 }
